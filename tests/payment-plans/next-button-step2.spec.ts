474b551de82b2f7677ae9258cbacbbb190c8a488
Jyoti Sahoo <jyotishankarsahoo86@gmail.com>
2025-10-13 09:57:13 -0500

    Payment to review page navigation

--- tests/payment-plans/next-button-step2.spec.ts
+++ tests/payment-plans/next-button-step2.spec.ts
@@ -1,15 +1,18 @@
 import { test, expect } from "../../utilities/sep-test-utilities";
 import { CommonUI } from "../../utilities/sep-test-utilities";
 import { PaymentPlanPage } from "../../pages/PaymentPlanPage";
+import { StartApplicationPage } from "../../pages/StartApplicationPage";
 
 test.describe(
     "Next button on selecting payment page",
     { tag: "@sep20" },
     () => {
         let paymentPlanPage: PaymentPlanPage;
+        let startApplicationPage: StartApplicationPage;
 
         test.beforeEach(async ({ page }) => {
             paymentPlanPage = new PaymentPlanPage(page);
+            startApplicationPage = new StartApplicationPage(page);
             await CommonUI.completeStartApplicationForm(
                 page,
                 "Jyoti",
@@ -22,11 +25,27 @@ test.describe(
         test("Verify that the next button is displayed", async ({ page }) => {
             await expect(paymentPlanPage.inactiveNextButton).toBeVisible();
         });
-
         test("Verify that the next button is disabled by default", async ({
             page,
         }) => {
             await expect(paymentPlanPage.inactiveNextButton).toBeDisabled();
+        });
+        test("Verify clicking on next button navigates to step-3", async ({
+            page,
+        }) => {
+            await paymentPlanPage.selectPaymentPlan("upfront");
+            await paymentPlanPage.clickNextButton();
+            await expect(
+                startApplicationPage.startApplicationStepCircle
+            ).toHaveCSS("background-color", "rgb(172, 245, 138)");
+            await expect(startApplicationPage.paymentPlanStepCircle).toHaveCSS(
+                "background-color",
+                "rgb(172, 245, 138)"
+            );
+            await expect(startApplicationPage.reviewStepCircle).toHaveCSS(
+                "background-color",
+                "rgb(1, 201, 255)"
+            );
         });
     }
 );
